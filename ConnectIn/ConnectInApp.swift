6b0aae23526c8680ce88bbd62c3382cd4e08eb20
Fathima KK <fathimakk003@gmail.com>
2026-05-03 19:08:25 -0700

    Initial Commit

---
+++ ConnectIn/ConnectInApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectInApp.swift
+//  ConnectIn
+//
+//  Created by Fathima K K on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ConnectInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
